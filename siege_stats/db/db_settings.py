8447b8fc7e6c335c609d8a459523c9d9f63175bc
Ross Alexandra <Ross-Alexandra@outlook.com>
2021-03-19 11:23:42 -0700

    Added db_settings.py, it now uses env vars.

---
+++ siege_stats/db/db_settings.py
@@ -0,0 +1,7 @@
+import os
+
+host = os.getenv("POSTGRESHOST")
+database = os.getenv("SIEGEANALYZERDATABASE")
+db_user = os.getenv("SIEGEANALYZERDBUSER")
+db_pass = os.getenv("SIEGEANALYZERDBPASS") 
+port = os.getenv("POSTGRESPORT")
