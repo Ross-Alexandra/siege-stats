3f32261972f131a7ae3bb81fec4e10f7a3592e23
Ross Alexandra <Ross-Alexandra@outlook.com>
2021-03-20 14:16:09 -0700

    Fixed syntax errors with db.sql.
     - File can now be used to generate the
       database from scratch in a ready-state.

--- siege_stats/db/db.sql
+++ siege_stats/db/db.sql
@@ -3,9 +3,15 @@ CREATE TABLE players (
     player_name VARCHAR(20) NOT NULL
 );
 
+CREATE TABLE team_names (
+    team_id serial PRIMARY KEY,
+    team_name VARCHAR(50)
+);
+
 CREATE TABLE teams (
     team_id serial,
-    player_id serial,
+    player_id integer,
+    name_id integer REFERENCES team_names(team_id),
     PRIMARY KEY(team_id, player_id),
     CONSTRAINT plaryer_exists FOREIGN KEY(player_id) REFERENCES players(player_id)
 );
@@ -23,23 +29,23 @@ CREATE TABLE match_types (
 CREATE TABLE matches (
     match_id serial PRIMARY KEY,
     analyst_identifier VARCHAR(40),
-    team_id serial,
-    map_id serial,
-    match_type_id serial,
+    team_id integer,
+    map_id integer,
+    match_type_id integer,
     rounds_won integer,
     rounds_lost integer,
     score_at_half integer,
     attacker_start boolean,
-    CONSTRAINT matches_map_exists FOREIGN KEY(map_id) REFERENCES maps(map_id)
-    CONSTRAINT matches_match_type_exists FOREIGN KEY(match_type_id) REFERENCES match_types(type_id)
+    CONSTRAINT matches_map_exists FOREIGN KEY(map_id) REFERENCES maps(map_id),
+    CONSTRAINT matches_match_type_exists FOREIGN KEY(match_type_id) REFERENCES match_types(type_id),
     CONSTRAINT analyst_identifier_is_unique UNIQUE(analyst_identifier)
 );
 
 CREATE TABLE stats (
     stat_id serial PRIMARY KEY,
-    player_id serial,
-    match_id serial,
-    map_id serial,
+    player_id integer,
+    match_id integer,
+    map_id integer,
     rating real,
     attack_rating real,
     defence_rating real,
