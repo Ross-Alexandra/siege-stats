a4afa9f7c23a46d1ce7059f6c4262f0452e40a03
Ross Alexandra <Ross-Alexandra@outlook.com>
2021-03-20 14:41:27 -0700

    Bug Fixes
     - Fixed a bug which caused teams not to be created properly.
    
     - Added an error occurred message.

--- siege_stats/bot/bot.py
+++ siege_stats/bot/bot.py
@@ -14,75 +14,80 @@ class StatsBot(discord.Client):
 
     async def on_message(self, message: discord.Message):
 
-        # If AnalyticsBot has not been mentioned, then ignore the message.
-        if 'AnalyticsBot' not in [mention.name for mention in message.mentions]:
-            print("Message did not mention AnalyticsBot.")
-            return
+        try:
+            # If AnalyticsBot has not been mentioned, then ignore the message.
+            if 'AnalyticsBot' not in [mention.name for mention in message.mentions]:
+                print("Message did not mention AnalyticsBot.")
+                return
 
-        # If the message has no attachments, ignore it.
-        if len(message.attachments) == 0 or not all([attachment.url.endswith('.csv') for attachment in message.attachments]):
-            print("No attachments, or non-csv file included.")
-            return
-    
-        # Define the match_type for use in the db.
-        if "scrim" in message.channel.name:
-            match_type = "scrim"
-        elif "qual" in str(message.channel.name):
-            match_type = "qualifier"
-        elif "league" in str(message.channel.name):
-            match_type = "league"
-        else:
-            print(f"csv uploaded in invalid text channel: {message.channel.name}. Ignoring.")
-            return
+            # If the message has no attachments, ignore it.
+            if len(message.attachments) == 0 or not all([attachment.url.endswith('.csv') for attachment in message.attachments]):
+                print("No attachments, or non-csv file included.")
+                return
+        
+            # Define the match_type for use in the db.
+            if "scrim" in message.channel.name:
+                match_type = "scrim"
+            elif "qual" in str(message.channel.name):
+                match_type = "qualifier"
+            elif "league" in str(message.channel.name):
+                match_type = "league"
+            else:
+                print(f"csv uploaded in invalid text channel: {message.channel.name}. Ignoring.")
+                return
 
-        # Create a DB connection and parse out match statistics.
-        db_conn = BotDB()
-        match_statistics = self._get_match_stats([attachment.url for attachment in message.attachments])
+            # Create a DB connection and parse out match statistics.
+            db_conn = BotDB()
+            match_statistics = self._get_match_stats([attachment.url for attachment in message.attachments])
 
-        # Create a dictionary to hold the args for creating a match, and create the match object.
-        kwargs = {
-            "analyst_identifier": match_statistics.match_data["matchId"], 
-            "map_str": match_statistics.match_data["mapString"],
-            "match_type": match_type, 
-            "rounds_won": match_statistics.match_data["roundsWon"], 
-            "rounds_lost": match_statistics.match_data["roundsLost"], 
-            "score_at_half": match_statistics.match_data["scoreAtHalf"], 
-            "attackers_start": match_statistics.match_data["start_attack"]
-        }
-        match_id, match_created = db_conn.add_match(**kwargs)
+            db_conn.add_team(match_statistics.player_stats.keys())
 
-        # If that match already exists, then ignore this row.
-        if not match_created:
-            print(f"Match already exists in database. Ignoring.")
-            return
-
-
-        for player_name, stat_object in match_statistics.player_stats.items():
-            # Create a dictionary to hold the args for creating a stats object and create the stats object.
+            # Create a dictionary to hold the args for creating a match, and create the match object.
             kwargs = {
-                "player": player_name,
-                "match_id": match_id, 
-                "map_string": match_statistics.match_data["mapString"],
-                "rating": stat_object.get_rating(),
-                "attack_rating": stat_object.get_attack_rating(),
-                "defence_rating": stat_object.get_defence_rating(),
-                "kill_differential": stat_object.get_kill_differential(),
-                "entry_differential": stat_object.get_entry_differential(),
-                "trade_differential": stat_object.get_trade_differential(),
-                "kost": stat_object.get_kost(),
-                "kills_per_round": stat_object.get_kills_per_round(),
-                "survival_percentage": stat_object.get_survival_percent(),
-                "headshot_percentage": stat_object.get_headshot_percent(),
-                "multi_kill_rounds": stat_object.get_multi_kill_rounds(),
-                "deaths": stat_object.get_deaths(),
-                "kills": stat_object.get_kills(),
-                "defuser_planted": stat_object.get_defuser_planted(),
-                "defuser_disabled": stat_object.get_defuser_disabled(),
-                "team_kills": stat_object.get_team_kills()
+                "analyst_identifier": match_statistics.match_data["matchId"], 
+                "map_str": match_statistics.match_data["mapString"],
+                "match_type": match_type, 
+                "rounds_won": match_statistics.match_data["roundsWon"], 
+                "rounds_lost": match_statistics.match_data["roundsLost"], 
+                "score_at_half": match_statistics.match_data["scoreAtHalf"], 
+                "attackers_start": match_statistics.match_data["start_attack"]
             }
-            db_conn.add_statistic(**kwargs)
+            match_id, match_created = db_conn.add_match(**kwargs)
 
-        message.channel.send(content="Finished processing your file :)")
+            # If that match already exists, then ignore this row.
+            if not match_created:
+                print(f"Match already exists in database. Ignoring.")
+                return
+
+
+            for player_name, stat_object in match_statistics.player_stats.items():
+                # Create a dictionary to hold the args for creating a stats object and create the stats object.
+                kwargs = {
+                    "player": player_name,
+                    "match_id": match_id, 
+                    "map_string": match_statistics.match_data["mapString"],
+                    "rating": stat_object.get_rating(),
+                    "attack_rating": stat_object.get_attack_rating(),
+                    "defence_rating": stat_object.get_defence_rating(),
+                    "kill_differential": stat_object.get_kill_differential(),
+                    "entry_differential": stat_object.get_entry_differential(),
+                    "trade_differential": stat_object.get_trade_differential(),
+                    "kost": stat_object.get_kost(),
+                    "kills_per_round": stat_object.get_kills_per_round(),
+                    "survival_percentage": stat_object.get_survival_percent(),
+                    "headshot_percentage": stat_object.get_headshot_percent(),
+                    "multi_kill_rounds": stat_object.get_multi_kill_rounds(),
+                    "deaths": stat_object.get_deaths(),
+                    "kills": stat_object.get_kills(),
+                    "defuser_planted": stat_object.get_defuser_planted(),
+                    "defuser_disabled": stat_object.get_defuser_disabled(),
+                    "team_kills": stat_object.get_team_kills()
+                }
+                db_conn.add_statistic(**kwargs)
+
+            await message.channel.send(content="Finished processing your file :)")
+        except Exception:
+            await message.channel.send(conent="An error occurred while processing the file :(")
 
     @staticmethod
     def _temp_download_file(url):
@@ -107,7 +112,6 @@ class StatsBot(discord.Client):
 
 if __name__ == "__main__":
     from siege_stats.db.db_settings import db_pass
-    print(f"Attempting connection with db_pass: {db_pass}")
 
     client = StatsBot()
     client.run(bot_settings.bot_token)
